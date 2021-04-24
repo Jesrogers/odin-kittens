0063355b9635d57040d3b971f42c056d228f4bab
Jesse Rogers <jesserileyrogers@gmail.com>
2021-04-24 18:20:20 -0400

    various controllers and form

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,15 +1,27 @@
 class KittensController < ApplicationController
+    include KittensHelper
+
     def index
         @kittens = Kitten.all
     end
 
     def show
+        @kitten = Kitten.find(params[:id])
     end
 
     def new
+        @kitten = Kitten.new
     end
 
     def create
+        @kitten = Kitten.new(kitten_params)
+
+        if @kitten.save
+            redirect_to root_path, notice: "New kitten created!"
+        else
+            flash.now.alert = "Error creating kitten"
+            render :new
+        end
     end
 
     def edit
