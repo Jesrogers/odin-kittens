bef04c34a860a87a16d8a83bac052432dbcd308b
Jesse Rogers <jesserileyrogers@gmail.com>
2021-04-24 17:50:37 -0400

    controller actions and form partial created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "KittensController#index"
+  root "kittens#index"
 
   resources :kittens
 end
